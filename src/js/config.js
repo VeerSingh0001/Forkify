45e21fcdf1d78e898d0f3893c6c1d594a6ecf8df
Dalveer Singh <singhdalveer247@gmail.com>
2023-09-27 16:43:00 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@
 ///////////////////////////////////////////
 
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
-export const API_KEY = "3d49a18a-9914-4159-88aa-2b514fcf0c40";
+export const API_KEY = "59d24a9a-f417-47a8-8c8a-61015ad0f044";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODEL_CLOSE_SEC = 2.5
